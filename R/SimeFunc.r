d139a35500926076fc8de5f5e1f8158a3f02fbfd
Dr Simon de Lestang <simon.delestang@fish.wa.gov.au>
2021-07-02 15:29:11 +0800

    Add files via upload

--- R/SimeFunc.r
+++ R/SimeFunc.r
@@ -1,22 +1,71 @@
-library(dplyr)
-library(magrittr)
-library(gplots)
+#' @import dplyr
+#' @import magrittr
+#' @import gplots
 
-## Set the library
+
+#' Set the library
+#'
+#' This sets the library to this link 
+#' @param there is none
+#' @return Does not reurn jack
+#' @examples 
+#' Dont need
+#' @export
 .libPaths('C:\\Users\\snd\\OneDrive - Department of Primary Industries and Regional Development\\Maths and Stats\\R.Librarys')
 
-## Useful functions
+#' Pulls out the day as a number
+#'
+#' This extracts a day from a date 
+#' @param x the date 
+#' @return Returns the day
+#' @examples 
+#' as.day("2014-02-14")
+#' @export
 as.day <-   function(x,fmat='%Y-%m-%d')    as.numeric(format(as.Date(x, format=fmat),'%d'))
+
+
+#' Pulls out the month as a number
+#'
+#' This extracts a month from a date 
+#' @param x the date 
+#' @return Returns the month
+#' @examples 
+#' as.month("2014-02-14")
+#' @export
 as.month <- function(x,fmat='%Y-%m-%d')    as.numeric(format(as.Date(x, format=fmat),'%m'))
+
+#' Pulls out the year as a number
+#'
+#' This extracts a year from a date 
+#' @param x the date 
+#' @return Returns the year
+#' @examples 
+#' as.year("2014-02-14")
+#' @export
 as.year <-  function(x,fmat='%Y-%m-%d')    as.numeric(format(as.Date(x, format=fmat),'%Y'))
+
+#' Change factor to number
+#'
+#' This Changes a factor to number
+#' @param x teh factor
+#' @return Returns a number
+#' @examples 
+#' numchar(factor)
+#' @export
 numchar <-  function(x) as.numeric(as.character(x))
 
 
-#New directory function
+#' New directory function
+#'
+#' This is a short cut that adds One Drive into the directory
+#' @param x the suffix of the address
+#' @return Returns a full address including OneDrive in full
+#' @examples 
+#' direct("lobster/Minor Stuff/MEY")
+#' @export
 direct <- function(x){
   paste("C:/Users/snd/OneDrive - Department of Primary Industries and Regional Development/",x,sep="")
 }
-
 
 
 #' Copy to Clipboard
